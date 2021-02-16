95025b24691ca584941e003e1857b863cfbda076
Pete VS <pete_vs88@me.com>
2021-02-16 13:21:42 -0500

    logger written

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,9 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+    const time = new Date().toISOString()
+    console.log(`${time} ${req.ip} ${req.method} ${req.url}`)
+
+    next()
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +19,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
