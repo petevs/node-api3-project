460c4179859c1c37cdc573c22e138928caddea44
Pete VS <pete_vs88@me.com>
2021-02-18 19:10:02 -0500

    made port dynamic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require your server and launch it
 const server = require("./api/server")
 
-const port = 4000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
